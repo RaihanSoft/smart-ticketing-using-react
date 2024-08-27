1d847a708fb3dfc6928bd70ff1cd68834056954c
Abu Raihan <137011693+RaihanSoft@users.noreply.github.com>
2024-08-27 18:33:03 +0600

    banner three card added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <div>
-      <section className="bg-[url(./assets/banner.png)] bg-cover mt-2 rounded-3xl w-11/12 mx-auto text-white bg-no-repeat bg-center">
+      <section className="bg-[url(./assets/banner.png)] bg-cover mt-2 rounded-3xl w-11/12 mx-auto text-white bg-no-repeat bg-center relative">
         <div className="text-center space-y-10 py-32">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold">
             End-to-End Travel with <br />{" "}
@@ -15,6 +15,45 @@ function Header() {
           <button className="btn bg-green-300 px-10 font-bold hover:bg-green-600 text-xl">
             Buy Ticket
           </button>
+        </div>
+
+        <div className="flex justify-center items-center gap-8 absolute -bottom-16 left-80">
+          {/* card one  */}
+          <div className="border-4 bg-white rounded-2xl p-5">
+            <div>
+              <img src="./assets/people.png" alt="" />
+            </div>
+
+            <div className="text-black">
+              <h1 className="text-5xl font-extrabold">500K</h1>
+              <h2 className="font-bold text-2xl">Registered users</h2>
+            </div>
+          </div>
+
+          {/* card one  */}
+          <div className="border-4 bg-white rounded-2xl p-5">
+            <div>
+              <img src="./assets/people.png" alt="" />
+            </div>
+
+            <div className="text-black">
+              <h1 className="text-5xl font-extrabold">500K</h1>
+              <h2 className="font-bold text-2xl">Registered users</h2>
+            </div>
+          </div>
+
+          {/* card one  */}
+
+          <div className="border-4 bg-white rounded-2xl p-5">
+            <div>
+              <img src="./assets/people.png" alt="" />
+            </div>
+
+            <div className="text-black">
+              <h1 className="text-5xl font-extrabold">500K</h1>
+              <h2 className="font-bold text-2xl">Registered users</h2>
+            </div>
+          </div>
         </div>
       </section>
     </div>
