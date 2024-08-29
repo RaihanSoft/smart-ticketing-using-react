9f27cf211d5a6787256f2be0b360a7b0e0c65541
Abu Raihan <137011693+RaihanSoft@users.noreply.github.com>
2024-08-29 17:46:41 +0600

    responsive for mobile and lapton devices

--- src/components/PH/PH.jsx
+++ src/components/PH/PH.jsx
@@ -4,188 +4,190 @@ function PH() {
   return (
     <div className='border-t-4  rounded-[10%] border-green-400 bg-gray-100'>
       <section className='w-11/12 mx-auto '>
-<div className='text-center pt-10 '>
+
+        <div className='text-center pt-10 '>
           <h1 className='text-4xl font-bold'>P.H Paribahan</h1>
-          <p className='mt-3'>Yes, you can run unit tests and view the results directly within the app. The integrated 
-          testing features allow for a streamlined .</p>
-</div>
+          <p className='mt-3'>Yes, you can run unit tests and view the results directly within the app. The integrated
+            testing features allow for a streamlined .</p>
+        </div>
 
-<div className='flex justify-between my-10 p-10 bg-white rounded-2xl'>
-
-
-  {/* header div  */}
-<div className=''>
-  <div className='flex justify-between gap-24 '>
-      <div className=''>
-        <h1 className='text-3xl font-bold'>Greenline Paribahan</h1>
-        <p className='font-bold text-xl'>Coach-009-WEB ! AC_Business</p>
-      </div>
-    
-      <div>
-        <button className='btn bg-green-300'>40 Seats left</button>
-      </div>
-    
-  </div>
-
-<div className=' bg-gray-100 mt-10 p-10  rounded-xl space-y-10 text-xl font-bold'>
-  
-    <div className='flex gap-[600px]'>
-      <div>
-     <h1> Route</h1>
-      </div>
-      <div>
-     <h1>Dhaka - Sylhet</h1>
-      </div>
-  
-    </div>
-  
-    <div className='flex gap-[570px]'>
-      <div>
-     <h1>Departure Time</h1>
-      </div>
-      <div>
-     <h1>9:00 PM</h1>
-      </div>
-  
-    </div>
-  
-    <div className='flex gap-16'>
-    <button  className='btn shadow-lg'> Boarding Point - Laxmipur</button> 
-    <button  className='btn shadow-lg'> Dropping Point - Bogura</button> 
-    <button  className='btn shadow-lg'> Est. Time - 11 Hour</button> 
-  
-  
-    </div>
-  
-</div>
+        <div className='md:flex justify-between my-10 p-10 bg-white rounded-2xl'>
 
 
 
+          {/* header div  */}
+          <div className=''>
+            <div className='md:flex justify-between gap-24 '>
+              <div className=''>
+                <h1 className='text-3xl font-bold'>Greenline Paribahan</h1>
+                <p className='font-bold text-xl'>Coach-009-WEB ! AC_Business</p>
+              </div>
 
-  
-</div>
+              <div>
+                <button className='btn bg-green-300'>40 Seats left</button>
+              </div>
+
+            </div>
+
+            <div className=' bg-gray-100 mt-10 p-10  rounded-xl space-y-10 text-xl font-bold'>
+
+              <div className=' md:flex gap-[600px]'>
+                <div>
+                  <h1> Route</h1>
+                </div>
+                <div>
+                  <h1>Dhaka - Sylhet</h1>
+                </div>
+
+              </div>
+
+              <div className='md:flex gap-[570px]'>
+                <div>
+                  <h1>Departure Time</h1>
+                </div>
+                <div>
+                  <h1>9:00 PM</h1>
+                </div>
+
+              </div>
+
+              <div className='md:flex gap-16'>
+                <button className='btn shadow-lg'> Boarding Point - Laxmipur</button>
+                <button className='btn shadow-lg'> Dropping Point - Bogura</button>
+                <button className='btn shadow-lg'> Est. Time - 11 Hour</button>
 
 
+              </div>
 
-<div className=' border-l-4 border-l-green-400 border-dashed pl-40 text-center flex flex-col justify-center'>
-
-  <h1 className='text-3xl font-extrabold'>550 TAKA</h1>
-  <p className='font-bold text-2xl'>per seat</p>
-</div>
+            </div>
 
 
 
 
 
+          </div>
 
 
 
-</div>
+          <div className=' border-l-4 border-l-green-400 border-dashed pl-40 text-center flex flex-col justify-center'>
+
+            <h1 className='text-3xl font-extrabold'>550 TAKA</h1>
+            <p className='font-bold text-2xl'>per seat</p>
+          </div>
+
+
+
+
+
+
+
+
+        </div>
       </section>
 
 
 
       {/* 2nd part  */}
-{/* main div  */}
-      <div className='mx-auto  w-11/12 py-5 bg-white rounded-2xl px-3 flex gap-10'>
+      {/* main div  */}
+      <div className='mx-auto  w-11/12 py-5 bg-white rounded-2xl px-3 md:flex gap-10'>
 
         {/* left  */}
         <div className='space-y-10 border-r-4 pr-12 border-green-400 border-dotted'>
           <h1 className='font-bold text-2xl'>Select Your Seat</h1>
-          <div className='flex justify-between'>
+          <div className='md:flex justify-between'>
             <h1>Available</h1>
             <h1>Selected</h1>
           </div>
 
 
-          <div className='flex gap-32 justify-center items-center '>
-          <h1 className='text-xl' >A</h1>
-          <button className='btn btn-success text-white text-xl ' >A1</button>
-          <button className='btn btn-success text-white text-xl ' >A2</button>
-          <button className='btn btn-success text-white text-xl ' >A3</button>
-          <button className='btn btn-success text-white text-xl ' >A4</button>
-        </div>
+          <div className='md:flex space-x-3 md:space-x-0 gap-32 justify-center items-center '>
+            <h1 className='text-xl' >A</h1>
+            <button className='btn btn-success text-white text-xl ' >A1</button>
+            <button className='btn btn-success text-white text-xl ' >A2</button>
+            <button className='btn btn-success text-white text-xl ' >A3</button>
+            <button className='btn btn-success text-white text-xl ' >A4</button>
+          </div>
 
 
-          <div className='flex gap-32 justify-center items-center '>
-          <h1 className='text-xl' >A</h1>
-          <button className='btn btn-success text-white text-xl ' >A1</button>
-          <button className='btn btn-success text-white text-xl ' >A2</button>
-          <button className='btn btn-success text-white text-xl ' >A3</button>
-          <button className='btn btn-success text-white text-xl ' >A4</button>
-        </div>
+          <div className='md:flex space-x-3 md:space-x-0 gap-32 justify-center items-center '>
+            <h1 className='text-xl' >A</h1>
+            <button className='btn btn-success text-white text-xl ' >A1</button>
+            <button className='btn btn-success text-white text-xl ' >A2</button>
+            <button className='btn btn-success text-white text-xl ' >A3</button>
+            <button className='btn btn-success text-white text-xl ' >A4</button>
+          </div>
 
 
-          <div className='flex gap-32 justify-center items-center '>
-          <h1 className='text-xl' >A</h1>
-          <button className='btn btn-success text-white text-xl ' >A1</button>
-          <button className='btn btn-success text-white text-xl ' >A2</button>
-          <button className='btn btn-success text-white text-xl ' >A3</button>
-          <button className='btn btn-success text-white text-xl ' >A4</button>
-        </div>
+          <div className='md:flex space-x-3 md:space-x-0 gap-32 justify-center items-center '>
+            <h1 className='text-xl' >A</h1>
+            <button className='btn btn-success text-white text-xl ' >A1</button>
+            <button className='btn btn-success text-white text-xl ' >A2</button>
+            <button className='btn btn-success text-white text-xl ' >A3</button>
+            <button className='btn btn-success text-white text-xl ' >A4</button>
+          </div>
 
 
-          <div className='flex gap-32 justify-center items-center '>
-          <h1 className='text-xl' >A</h1>
-          <button className='btn btn-success text-white text-xl ' >A1</button>
-          <button className='btn btn-success text-white text-xl ' >A2</button>
-          <button className='btn btn-success text-white text-xl ' >A3</button>
-          <button className='btn btn-success text-white text-xl ' >A4</button>
-        </div>
+          <div className='md:flex space-x-3 md:space-x-0 gap-32 justify-center items-center '>
+            <h1 className='text-xl' >A</h1>
+            <button className='btn btn-success text-white text-xl ' >A1</button>
+            <button className='btn btn-success text-white text-xl ' >A2</button>
+            <button className='btn btn-success text-white text-xl ' >A3</button>
+            <button className='btn btn-success text-white text-xl ' >A4</button>
+          </div>
 
 
-          <div className='flex gap-32 justify-center items-center '>
-          <h1 className='text-xl' >A</h1>
-          <button className='btn btn-success text-white text-xl ' >A1</button>
-          <button className='btn btn-success text-white text-xl ' >A2</button>
-          <button className='btn btn-success text-white text-xl ' >A3</button>
-          <button className='btn btn-success text-white text-xl ' >A4</button>
-        </div>
+          <div className='md:flex space-x-3 md:space-x-0 gap-32 justify-center items-center '>
+            <h1 className='text-xl' >A</h1>
+            <button className='btn btn-success text-white text-xl ' >A1</button>
+            <button className='btn btn-success text-white text-xl ' >A2</button>
+            <button className='btn btn-success text-white text-xl ' >A3</button>
+            <button className='btn btn-success text-white text-xl ' >A4</button>
+          </div>
 
 
-          <div className='flex gap-32 justify-center items-center '>
-          <h1 className='text-xl' >A</h1>
-          <button className='btn btn-success text-white text-xl ' >A1</button>
-          <button className='btn btn-success text-white text-xl ' >A2</button>
-          <button className='btn btn-success text-white text-xl ' >A3</button>
-          <button className='btn btn-success text-white text-xl ' >A4</button>
-        </div>
+          <div className='md:flex space-x-3 md:space-x-0 gap-32 justify-center items-center '>
+            <h1 className='text-xl' >A</h1>
+            <button className='btn btn-success text-white text-xl ' >A1</button>
+            <button className='btn btn-success text-white text-xl ' >A2</button>
+            <button className='btn btn-success text-white text-xl ' >A3</button>
+            <button className='btn btn-success text-white text-xl ' >A4</button>
+          </div>
 
 
-          <div className='flex gap-32 justify-center items-center '>
-          <h1 className='text-xl' >A</h1>
-          <button className='btn btn-success text-white text-xl ' >A1</button>
-          <button className='btn btn-success text-white text-xl ' >A2</button>
-          <button className='btn btn-success text-white text-xl ' >A3</button>
-          <button className='btn btn-success text-white text-xl ' >A4</button>
-        </div>
+          <div className='md:flex space-x-3 md:space-x-0 gap-32 justify-center items-center l'>
+            <h1 className='text-xl' >A</h1>
+            <button className='btn btn-success text-white text-xl ' >A1</button>
+            <button className='btn btn-success text-white text-xl ' >A2</button>
+            <button className='btn btn-success text-white text-xl ' >A3</button>
+            <button className='btn btn-success text-white text-xl ' >A4</button>
+          </div>
 
 
-          <div className='flex gap-32 justify-center items-center '>
-          <h1 className='text-xl' >A</h1>
-          <button className='btn btn-success text-white text-xl ' >A1</button>
-          <button className='btn btn-success text-white text-xl ' >A2</button>
-          <button className='btn btn-success text-white text-xl ' >A3</button>
-          <button className='btn btn-success text-white text-xl ' >A4</button>
-        </div>
+          <div className='md:flex space-x-3 md:space-x-0 gap-32 justify-center items-center '>
+            <h1 className='text-xl' >A</h1>
+            <button className='btn btn-success text-white text-xl ' >A1</button>
+            <button className='btn btn-success text-white text-xl ' >A2</button>
+            <button className='btn btn-success text-white text-xl ' >A3</button>
+            <button className='btn btn-success text-white text-xl ' >A4</button>
+          </div>
 
 
-          <div className='flex gap-32 justify-center items-center '>
-          <h1 className='text-xl' >A</h1>
-          <button className='btn btn-success text-white text-xl ' >A1</button>
-          <button className='btn btn-success text-white text-xl ' >A2</button>
-          <button className='btn btn-success text-white text-xl ' >A3</button>
-          <button className='btn btn-success text-white text-xl ' >A4</button>
-        </div>
+          <div className='md:flex space-x-3 md:space-x-0 gap-32 justify-center items-center '>
+            <h1 className='text-xl' >A</h1>
+            <button className='btn btn-success text-white text-xl ' >A1</button>
+            <button className='btn btn-success text-white text-xl ' >A2</button>
+            <button className='btn btn-success text-white text-xl ' >A3</button>
+            <button className='btn btn-success text-white text-xl ' >A4</button>
+          </div>
 
 
-          <div className='flex gap-32 justify-center items-center '>
-          <h1 className='text-xl' >A</h1>
-          <button className='btn btn-success text-white text-xl ' >A1</button>
-          <button className='btn btn-success text-white text-xl ' >A2</button>
-          <button className='btn btn-success text-white text-xl ' >A3</button>
-          <button className='btn btn-success text-white text-xl ' >A4</button>
-        </div>
+          <div className='md:flex space-x-3 md:space-x-0 gap-32 justify-center items-center '>
+            <h1 className='text-xl' >A</h1>
+            <button className='btn btn-success text-white text-xl ' >A1</button>
+            <button className='btn btn-success text-white text-xl ' >A2</button>
+            <button className='btn btn-success text-white text-xl ' >A3</button>
+            <button className='btn btn-success text-white text-xl ' >A4</button>
+          </div>
 
         </div>
 
@@ -193,86 +195,86 @@ function PH() {
 
 
         {/* right  */}
-       
-         <div className=''>
-            <h1 className='text-2xl font-bold'>Select Your Seat</h1>
-            <div className='bg-gray-200 mt-16 space-y-10 p-5 rounded-xl'>
-  
-            <div className='flex gap-40'>
+
+        <div className=''>
+          <h1 className='text-2xl font-bold'>Select Your Seat</h1>
+          <div className='bg-gray-200 mt-16 space-y-10 p-5 rounded-xl'>
+
+            <div className='md:flex gap-40'>
               <p className='text-xl font-bold'>seat</p>
               <p className='text-xl font-bold' >class</p>
               <p className='text-xl font-bold' >price</p>
             </div>
-  
-            <div className='flex gap-40'>
+
+            <div className='md:flex gap-40'>
               <p className=''>C2</p>
               <p>Economoy </p>
               <p>550</p>
             </div>
-  
-            <div className='flex gap-40'>
+
+            <div className='md:flex gap-40'>
               <p>C2</p>
               <p>Economoy </p>
               <p>550</p>
             </div>
-  
-            <div className='flex gap-72 border-t-4 py-4 border-white'>
+
+            <div className='md:flex gap-72 border-t-4 py-4 border-white'>
               <p>Total Price</p>
               <p>BDT 1100 </p>
             </div>
-  
+
             <div>
-              <input className='w-[400px] h-10 p-2' placeholder='Have any copupon?' type="text" name="" id="" />
+              <input className='md:w-[400px] w-full h-10 p-2' placeholder='Have any copupon' type="text" name="" id="" />
             </div>
-  
-            <div className='flex gap-72 border-t-4'>
+
+            <div className='md:flex gap-72 border-t-4'>
               <p>Grand Total</p>
               <p>BDT 1100</p>
             </div>
-  
-  
+
+
           </div>
-{/* second  */}
+          {/* second  */}
 
           <div className='space-y-10'>
-<div className='mt-10'>
-  
+            <div className='mt-10'>
+
               <h1>Passenger Name*</h1>
               <div>
-                <input className='w-[400px] h-10 p-2' placeholder='Have any copupon?' type="text" name="" id="" />
+                <input className='w-full h-10 p-2' placeholder='Have any copupon' type="text" name="" id="" />
               </div>
-</div >
+            </div >
 
-<div >
-  
+            <div >
+
               <h1>phone Number*</h1>
               <div>
                 <input className='w-[400px] h-10 p-2' placeholder='Have any copupon?' type="text" name="" id="" />
               </div>
-</div>
-<div>
-  
+            </div>
+            <div>
+
               <h1>Email ID</h1>
               <div>
-                <input className='w-[400px] h-10 p-2' placeholder='Have any copupon?' type="text" name="" id="" />
+                <input className='md:w-[400px] w-full h-10 p-2' placeholder='Have any copupon?' type="text" name="" id="" />
               </div>
-</div>
+            </div>
 
-<div>
-  <button className='btn btn-success w-full text-white'>Next</button>
-</div>
+            <div>
+              <button className='btn btn-success w-full text-white'>Next</button>
+            </div>
 
-<div className='flex justify-center gap-20 font-bold underline'>
-  <p>Terms and Conditions</p>
-<p>Cancellation Policy </p>
+            <div className='md:flex justify-center gap-20 font-bold underline'>
+              <p>Terms and Conditions</p>
+              <p>Cancellation Policy </p>
 
 
-</div>
+            </div>
 
 
           </div>
-  
-         </div>
+
+        </div>
 
 
 
@@ -282,7 +284,7 @@ function PH() {
       </div>
 
 
-      
+
     </div>
   )
 }
